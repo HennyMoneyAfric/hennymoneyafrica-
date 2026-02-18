071594d27f09d71bc1e077acfbeed041718c9e20
HennyMoneyAfric <hennymoneyafric@gmail.com>
2026-02-18 03:59:50 +0000

    Delete script.js
    Remove old script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Register service worker for PWA
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js')
-    .then(() => console.log("Service Worker Registered"))
-    .catch(err => console.log("Service Worker failed:", err));
-}
